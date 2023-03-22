f5156877388e183427ab583375a03f282fb92af5
pavlo.maksymov <pavlo.maksymov@spryker.com>
2023-03-22 22:49:32 +0100

    Improved with randomizer

--- bin/index.php
+++ bin/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Buck up!" . PHP_EOL;
+require __DIR__.'/../vendor/autoload.php';
+
+$phrases = [
+    "Buck up",
+    "Bee cool",
+    "Don't worry, be happy!",
+];
+
+$randomizer = new Randomizer\Randomizer();
+
+$phrase = $randomizer->randomString(...$phrases);
+
+echo $phrase . PHP_EOL;
